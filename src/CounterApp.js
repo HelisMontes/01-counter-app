d77fd26df95e5994164796dc8f4a6f5baec0ad6d
Helis Montes <helismontes@gmail.com>
2021-03-31 13:58:14 -0500

    Initial commit

---
+++ src/CounterApp.js
@@ -0,0 +1,28 @@
+import React, { useState }from 'react';
+import PropTypes from 'prop-types';
+
+export const CounterApp = ({ value = 11 }) => {
+    const [ count, setCount ] = useState( value ); // :[]
+    
+    const addEventSum = (e) => {
+        setCount( count + 1);
+        // setCount( (c) => c + 1 );
+    }
+    const addEventReset = (e) => setCount( value );
+    const addEventSubtract = (e) => count > 0 ? setCount( count - 1) : false;
+    return (
+        <>
+            <h1>CounterApp</h1>
+            <h2> { count } </h2>
+            <button onClick = { addEventSum }>+1</button>
+            <button onClick = { addEventReset }>Reset</button>
+            <button onClick = { addEventSubtract }>-1</button>
+            {/* Otra forma de hacer el event si no pasamos un parametro*/}
+            {/* <button onClick = { (e) => addEvent(e) }>+1</button> */}
+        </>
+    );
+}
+
+CounterApp.propTypes = {
+    value : PropTypes.number,
+}
---
+++ src/PrimeraApp.js
@@ -0,0 +1,35 @@
+//import React, { Fragment } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types'; 
+
+export const PrimerApp = ({ saludar = 'Hola Mundo...', subTitulo }) => {
+    //const saludar = `Hola Mundo`;
+    // const saludar2 = {
+    //     nombre: 'Helis',
+    //     apellido: 'Montes',
+    // };
+    return( 
+        <>
+            <h1>{ saludar }</h1>
+            {/* <pre> { JSON.stringify (saludar2, null, 3) }</pre> */}
+            <p>{ subTitulo }</p>
+        </>
+    );
+}
+
+PrimerApp.propTypes = {
+    saludar: PropTypes.string.isRequired,
+}
+
+PrimerApp.defaultProps = {
+    subTitulo: 'Soy un subtitulo'
+}
+
+/*
+export const PrimerApp = () => (
+    <Fragment>
+        <h1>Hola Mundo</h1>
+        <p> Mi primera APP</p>
+    </Fragment>
+);
+*/
